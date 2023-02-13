4c8e5401e70c07c2318352e66360b601dcabe7e5
Scott Wilkinson <swilkinson423@gmail.com>
2023-02-13 14:58:19 +0100

    added ability to create new shows

--- app.py
+++ app.py
@@ -788,21 +788,37 @@ def shows():
 
 @app.route('/shows/create')
 def create_shows():
-  # renders form. do not touch.
   form = ShowForm()
   return render_template('forms/new_show.html', form=form)
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
-  # called to create new shows in the db, upon submitting new show listing form
-  # TODO: insert form data as a new Show record in the db, instead
 
-  # on successful db insert, flash success
-  flash('Show was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Show could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-  return render_template('pages/home.html')
+  form = ShowForm(request.form)
+
+  error = False
+
+  try:
+    show = Show(
+      artist_id =   form.artist_id.data,
+      venue_id =    form.venue_id.data,
+      date =        form.start_time.data
+    )
+
+    db.session.add(show)
+    db.session.commit()
+
+    flash('Show was successfully listed!')
+  except:
+    db.session.rollback()
+    error = True
+    flash('An error occured. The show could not be listed.')
+    print(sys.exc_info())
+  if error:
+    return render_template('/forms/new_show.html')
+  else:
+    return render_template('pages/home.html')
+
 
 #  Delete Shows
 #  ----------------------------------------------------------------
